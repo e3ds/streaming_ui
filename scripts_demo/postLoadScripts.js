d7628d7f34c54c89944ad5af7be68e56477e0b95
muzaheed57 <muzaheed57@gmail.com>
2023-11-09 21:40:35 -0500

    no preload script

--- scripts_demo/postLoadScripts.js
+++ scripts_demo/postLoadScripts.js
@@ -1,25 +1,19 @@
+var e3ds_session_token =
+"170034345123277516995602857754108416/U2FsdGVkX19Q2IOljHMksrwCmSYUdoUsu+bh/1cGzCB79eVFvDTfEDHd/HpbrdbBanWRjsIAArz4/vt0oo+SHKpZiWPk0vTPR3Gx+eROnUB2bKtWY8CeU9OgDAI/zYZOmdXbKM6+kNH2U7WKzv6WoBEHiva3MSaPq3f5Y3mupn85l+U/XjpSUhm2y4y+1BO8FlFYwUbxClgSDaCevl/fpF2SQYv2rbbp9KIRqxs4F8CGKv2YOoc/LX9n/Les4FQCqpdh30o+krHS7pz1jxULnG2KgOpwkknNPpMBXuDH4kOk09BHYVcs2uKSVnxh4Z+Ag83oNBbkpUo/InrFWVLSVtLFQyaUxWBos1RxPma9Y5uYv9dTN5FhjOBFe6D2cSGsri9vUIfivBCF2ze4jGCeMS53cC8DobRNvBAdkKjT1xraEZSIQmemXI4GY6ISKtdlHGAXP0FIFJ4zXuBdfJ2e6zadf2mK6WiIy9tmiwF5y/w="
+
+var clientUserName ="demo"
 
 
-/* 				{
-					"core": 
-											  {
-												"domainxx": "connector_binary.eaglepixelstreaming.com",
-												"domain": "connector_ms6.eagle3dstreaming.com",
-												"userName": "demo",
-												"appName": "ue52",
-												"configurationName1": "E3DS-Iframe-Demo",
-												"configurationName": "0"
-												
-											  },
-				  
-					"configurationToOverride": 
-											  {
-												
-												"isAdminDebugging": 1
-											  }
-				} */
 
-// NOTE: Probably this is more suitable for postLoadScript??? really ??? lets talk about it . give me a call
+window.onload = function onloadOfHtml() 
+				{
+					
+					e3ds_controller.main(e3ds_session_token,clientUserName)
+					
+				}
+
+
+
 e3ds_controller.callbacks.onDataChannelOpen=function ()
 {
     console.log("ob-onDataChannelOpen");
@@ -83,36 +77,10 @@ e3ds_controller.callbacks.onReceivingAppStartingProgress=function (percent)
 e3ds_controller.callbacks.onHtmlBind=function ()
 {
     console.log("ob-onHtmlBind");
-    //changeConfigValue("useVOIP",1)
-	
-	
-
-let webrtcParamsSubmit2 = document.getElementById('webrtc-params-submit');
-	if (webrtcParamsSubmit2 !== null) {
-		webrtcParamsSubmit2.onclick = function (event) 
-		{
-			//console.log("=== Registered switchTo action, Value is: ", val);
-		let descriptor = document.getElementById('webrtc-max-fps-text').value;
-		/*let descriptor = {
-			Teleport: val
-		};*/
-		//emitUIInteraction(descriptor);
-		let descriptor2 = 
-						{
-							Teleport: document.getElementById('webrtc-max-fps-text').value
-						};
-		
-		
-		e3ds_controller.sendToUnreal(descriptor2)
-		};
-	}
-
+   
 
 }
 
-window.onload = function onloadOfHtml() 
-				{
-					//main()
-					e3ds_controller.main(e3ds_session_token,clientUserName)
-					//e3ds_controller.sendToUnreal("dgdgdgd")
-				}
+
+
+
